d180f0ecd4209dd63e200066fb10f244b1ff4069
Shu-YuanYang <zantgoron@gmail.com>
2025-05-20 13:48:49 -0500

    Implemented message backlog resolve procedure.

--- migrations/00009_add_communications_schema.sql
+++ migrations/00009_add_communications_schema.sql
@@ -70,6 +70,50 @@ $$;
 -- +goose StatementEnd
 
 
+
+-- +goose StatementBegin
+CREATE PROCEDURE communications.resolve_message_backlog(
+	target_backlog_id STRING,
+    target_message_id STRING,
+    target_edit_by STRING
+)
+LANGUAGE PLpgSQL AS $$
+DECLARE
+    message_userid STRING := NULL;
+	backlog_userid STRING := NULL;
+	backlog_message_type STRING := NULL;
+BEGIN
+	-- Query backlog spec user and message type
+	SELECT userid, message_type INTO backlog_userid, backlog_message_type
+	FROM communications.tb_message_backlog_spec
+	WHERE spec_id IN (
+		SELECT B.spec_id FROM communications.tb_message_backlog B 
+		WHERE B.backlog_id = target_backlog_id
+	);
+	
+	-- Validate message user and spec user
+	IF backlog_message_type = 'EMAIL' THEN
+		SELECT uploaded_by INTO message_userid 
+		FROM communications.tb_email_header
+		WHERE email_id = target_message_id;
+	ELSE 
+		RAISE EXCEPTION 'Backlog message type not found!';
+	END IF;
+
+	IF message_userid IS NULL OR message_userid <> backlog_userid THEN
+		RAISE EXCEPTION 'Unauthorized backlog resolve operation!';
+	END IF;
+
+    -- Update backlog status as 'RESOLVED'
+	UPDATE communications.tb_message_backlog
+	SET status = 'RESOLVED', note = backlog_message_type || ' ID: ' || target_message_id, edit_by = target_edit_by, edit_time = CURRENT_TIMESTAMP
+	WHERE backlog_id = target_backlog_id;
+	
+END;
+$$;
+-- +goose StatementEnd
+
+
 -- +goose StatementBegin
 CREATE FUNCTION communications.get_message_backlog_by_user(
     target_userid STRING,
@@ -90,6 +134,7 @@ $$;
 -- +goose Down
 DROP FUNCTION IF EXISTS communications.get_message_backlog_by_user;
 
+DROP PROCEDURE IF EXISTS communications.resolve_message_backlog;
 DROP PROCEDURE IF EXISTS communications.update_message_backlog_statuses;
 
 DROP TABLE IF EXISTS communications.tb_message_backlog;
